23f6ab81909a1f190721be85daf12a871ba6f116
Bob Brown <robertbernardbrown@gmail.com>
2018-11-02 18:50:20 -0400

    adds graphiql to project

--- server/app.js
+++ server/app.js
@@ -3,10 +3,11 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 
 const app = express();
-const port = 4000;
+const port = 3000;
 
-app.use('/graphql'.graphqlHTTP({
-    schema
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
 }))
 
 app.listen(port, () => {
