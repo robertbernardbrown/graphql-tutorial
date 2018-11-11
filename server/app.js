ad3f546dd38477519616834a259fc77606a5276b
Bob Brown <robertbernardbrown@gmail.com>
2018-11-11 15:24:26 -0500

    adds mlab to gql tutorial

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const user = 'bob123';
+const pw = 'Billybob123!'
 
 const app = express();
 const port = 3000;
+
+mongoose.connect(`mongodb://${user}:${pw}@ds219191.mlab.com:19191/gqltutorial`);
+mongoose.connection.once('open', () => {
+    console.log('connected to mlab')
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
