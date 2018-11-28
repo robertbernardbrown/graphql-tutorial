5c598267f70dab9bcb6481348571fc5b1df5cf23
Bob Brown <robertbernardbrown@gmail.com>
2018-11-27 22:19:58 -0500

    adds apolloclient and apolloprovider to project and wraps code

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import BookList from './components/BookList';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql'
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Reading List</h1>
-        <BookList/>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <h1>Reading List</h1>
+          <BookList/>
+        </div>
+      </ApolloProvider>
     );
   }
 } 
