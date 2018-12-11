54c496b2ec48178ee8684ade14029a64b433873e
Bob Brown <robertbernardbrown@gmail.com>
2018-12-10 22:12:39 -0500

    creates addBook component and displays a form that shows auto populated authors to users

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
@@ -14,6 +15,7 @@ class App extends Component {
         <div className="App">
           <h1>Reading List</h1>
           <BookList/>
+          <AddBook/>
         </div> 
       </ApolloProvider>
     );
